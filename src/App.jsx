5e9078ecee416d0b23f9aaee053641cf0deb18b1
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-05-02 19:25:32 +0300

    Тестовое задание

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,6 @@ function App() {
               <a href="https://spmipk.com/"><img alt="logo" src={logo}/></a>
             </div>
             <div className={styles.socialMedia}>
-              <a href="https://vk.me/spb.mipk?roistat_visit=139310" target="_blank">
                 <img alt="vk-icon" width="30" src={vkIcon } />
               </a>
               <a href="https://wa.me/+79210975015?roistat_visit=139310&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9C%D0%BD%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%B0%20%D0%BA%D0%BE%D0%BD%D1%81%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%86%D0%B8%D1%8F.%20%D0%9C%D0%BE%D0%B9%20%D0%BD%D0%BE%D0%BC%D0%B5%D1%80%20%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8F:%20127929" target="_blank">
